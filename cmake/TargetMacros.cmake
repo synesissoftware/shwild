58fda3a03a247cffdb650cebc18b3572e1d0ec77
Matt Wilson <matthew.wilson.synesis@gmail.com>
2025-08-30 10:59:07 +1000

    CTest compatibility

--- cmake/TargetMacros.cmake
+++ cmake/TargetMacros.cmake
@@ -1,16 +1,5 @@
 
-function(define_automated_test_program program_name entry_point_source_name)
-
-	add_executable(${program_name}
-		${entry_point_source_name}
-	)
-
-	target_link_libraries(${program_name}
-		PRIVATE
-			core
-			$<$<BOOL:${BDUT_FOUND}>:BDUT::BDUT>
-			$<$<BOOL:${STLSoft_FOUND}>:STLSoft::STLSoft>
-	)
+macro(define_target_compile_options target_name)
 
 	set(X_GCC_CUSTOM_WARNINGS_ "")
 
@@ -30,21 +19,40 @@ function(define_automated_test_program program_name entry_point_source_name)
 			endforeach()
 	endif()
 
-	target_compile_options(${program_name} PRIVATE
-		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-			-Werror -Wall -Wextra -pedantic
+	target_compile_options(${target_name}
+		PRIVATE
+			$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
+				-Werror -Wall -Wextra -pedantic
 
-			${GCC_WARN_NO_cxx11_long_long}
 			${X_GCC_CUSTOM_WARNINGS_}
-		>
-		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-			-Wno-unused-lambda-capture
 		>
 		$<$<CXX_COMPILER_ID:MSVC>:
 			/WX /W4
 
 			${X_MSVC_CUSTOM_WARNINGS_}
 		>
+	)
+endmacro(define_target_compile_options)
+
+
+function(define_automated_test_program program_name entry_point_source_name)
+
+	add_executable(${program_name}
+		${entry_point_source_name}
+	)
+
+	target_link_libraries(${program_name}
+		PRIVATE
+			core
+			$<$<BOOL:${BDUT_FOUND}>:BDUT::BDUT>
+			$<$<BOOL:${STLSoft_FOUND}>:STLSoft::STLSoft>
+	)
+
+	define_target_compile_options(${program_name})
+
+	add_test(
+		NAME ${program_name}
+		COMMAND ${program_name}
 	)
 endfunction(define_automated_test_program)
 
@@ -61,40 +69,7 @@ function(define_example_program program_name entry_point_source_name)
 		$<$<BOOL:${STLSoft_FOUND}>:STLSoft::STLSoft>
 	)
 
-	set(X_GCC_CUSTOM_WARNINGS_ "")
-
-	if(X_GCC_CUSTOM_WARNINGS_TO_BE_SUPPRESSED)
-		foreach(warning ${X_GCC_CUSTOM_WARNINGS_TO_BE_SUPPRESSED})
-
-			list(APPEND X_GCC_CUSTOM_WARNINGS_ "-Wno-${warning}")
-		endforeach()
-	endif()
-
-	set(X_MSVC_CUSTOM_WARNINGS_ "")
-
-	if(X_MSVC_CUSTOM_WARNINGS_TO_BE_SUPPRESSED)
-			foreach(warning ${X_MSVC_CUSTOM_WARNINGS_TO_BE_SUPPRESSED})
-
-					list(APPEND X_MSVC_CUSTOM_WARNINGS_ "/wd${warning}")
-			endforeach()
-	endif()
-
-	target_compile_options(${program_name} PRIVATE
-		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-			-Werror -Wall -Wextra -pedantic
-
-			${GCC_WARN_NO_cxx11_long_long}
-			${X_GCC_CUSTOM_WARNINGS_}
-		>
-		$<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>>:
-			-Wno-unused-lambda-capture
-		>
-		$<$<CXX_COMPILER_ID:MSVC>:
-			/WX /W4
-
-			${X_MSVC_CUSTOM_WARNINGS_}
-		>
-	)
+	define_target_compile_options(${program_name})
 endfunction(define_example_program)
 
 
