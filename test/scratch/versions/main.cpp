170c5a31f30dc819a7751ab3c6ab9f9205a1341f
Matt Wilson <matthew@synesis.com.au>
2026-09-17 16:24:35 +1000

    test(<lib>): rename scratch libver to versions
    Move test/scratch/libver to test/scratch/versions and retarget the
    executable as versions so C/C++ reporters match the Phase 3 freeze.

--- test/scratch/versions/main.cpp
+++ test/scratch/versions/main.cpp
@@ -13,7 +13,7 @@
 #include <stdlib.h>
 
 
-#define PROGRAM_NAME                                        "libver"
+#define PROGRAM_NAME                                        "versions"
 
 
 template<
